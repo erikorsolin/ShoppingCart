643084654a51e6c5e88e16ec78831d7dd3e36321
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-07 14:03:12 -0300

    correção de erro na classe Carrinho

--- FrontEnd.py
+++ FrontEnd.py
@@ -6,11 +6,10 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title('Shopping Cart')
+        self.minsize(400, 300)
         self.geometry('700x650')
-        customtkinter.set_appearance_mode('light')
-        self.frame = customtkinter.CTkFrame(master=self, width=650, height=600, corner_radius=15,fg_color='#0DD473')
+        customtkinter.set_appearance_mode('dark')
+        self.frame = customtkinter.CTkFrame(master=self, width=650, height=600, corner_radius=15,fg_color='#39537D')
         self.frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-        self.texto = customtkinter.CTkLabel(master=self.frame, fg_color='white')
         
-        self.entrada = customtkinter.CTkEntry(master=self.frame, placeholder_text='Digite seu nome')
-        self.entrada.pack(padx=650, pady=600)
+        
--- carrinho.py
+++ carrinho.py
@@ -1,31 +1,27 @@
 class Carrinho:
-    def __init__(self, nome_usuario: str) -> None:
-        self.__nome_usuario = nome_usuario
+    def __init__(self) -> None:
         self.__qtd_produtos = 0
         self.__preco_total = 0
         self.__produtos = dict()
     
-    def AdicionarProduto(self, nome: str, preco: float) -> str:
-        self.produtos[nome] = preco
-        self.qtd_produtos += 1
-        self.preco_total += self.produtos[nome]
-        r = 'Produto Adicionado'
-        return r
+    def AdicionarProduto(self, nome: str, preco: float) -> None:
+        self.__produtos[nome] = preco
+        self.__qtd_produtos += 1
+        self.__preco_total += self.__produtos[nome]
+        print('Produto Adicionado')
+        
 
-
-    def RemoverProduto(self, nome: str) -> str:
-        r = ''
+    def RemoverProduto(self, nome: str) -> None:
         if self.__verificar_produto(nome):
-            self.preco_total -= self.produtos[nome]
-            del self.produtos[nome]
-            r = 'Produto Removido'
-            self.qtd_produtos -= 1
+            self.__preco_total -= self.__produtos[nome]
+            del self.__produtos[nome]
+            print('Produto Removido')
+            self.__qtd_produtos -= 1
 
         else:
-            r = 'Produto não Está no Carrinho'
+           print('Produto não está no Carrinho')
 
-        return r
-    
+
     def __verificar_produto(self, nome: str) -> bool:
         if nome in self.__produtos.keys():
             return True
@@ -43,7 +39,3 @@ class Carrinho:
     
     def GetProdudos(self) -> dict:
         return self.__produtos
-
-    
-    def GetNomeUsuario(self) -> str:
-        return self.__nome_usuario
