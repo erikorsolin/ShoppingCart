ddf9b937523062b90ae706263e83013756d327a2
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-14 10:20:09 -0300

    fix: class name

--- front_end.py
+++ front_end.py
@@ -20,10 +20,29 @@ class App(customtkinter.CTk):
             text_color='#9CDFC3',
         )
         self.text1.place(x=115, y=90)
+
+        self.text2 = customtkinter.CTkLabel(
+            master=self.window1,
+            text='Login',
+            font=('Arial', 30),
+            text_color='#9CDFC3'
+        )
+        self.text2.place(x=267, y=250)
+
         self.entry1 = customtkinter.CTkEntry(
             master=self.window1,
             corner_radius=10,
             fg_color='#9CDFC3',
-            text_color='black'
+            text_color='black',
+            width=130
         )
         self.entry1.place(x=240, y=300)
+
+        self.entry2 = customtkinter.CTkEntry(
+            master=self.window1,
+            corner_radius=10,
+            fg_color='#9CDFC3',
+            text_color='black',
+            width=130
+        )
+        self.entry2.place(x=240, y=330 )
