90ed00146b03b1486c4f78427666c3264e8019fa
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-14 16:49:16 -0300

    criação de um novo frame

--- front_end.py
+++ front_end.py
@@ -12,6 +12,8 @@ class App(customtkinter.CTk):
         self.geometry('700x650')
         self.window1 = customtkinter.CTkFrame(master=self, width=650, height=600, corner_radius=15)
         self.window1.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.window2 = customtkinter.CTkFrame(master=self.window1, width=350, height=300, corner_radius=15, fg_color='#616178')
+        self.window2.place(x=135, y=200)
         self.text1 = customtkinter.CTkLabel(
             master=self.window1,
             text="Shopping Cart.",
@@ -23,36 +25,36 @@ class App(customtkinter.CTk):
         self.text1.place(x=115, y=90)
 
         self.text2 = customtkinter.CTkLabel(
-            master=self.window1,
+            master=self.window2,
             text='Log in',
             font=('Arial',  30),
         )
-        self.text2.place(x=267, y=250)
+        self.text2.place(x=130, y=30)
 
         self.entry1 = customtkinter.CTkEntry(
             placeholder_text='email',
             placeholder_text_color='#616178',
-            master=self.window1,
+            master=self.window2,
             corner_radius=10,
             text_color='white',
             width=200,
             height=35
         )
-        self.entry1.place(x=210, y=300)
+        self.entry1.place(x=75, y=80)
 
         self.entry2 = customtkinter.CTkEntry(
             placeholder_text='senha',
             placeholder_text_color='#616178',
-            master=self.window1,
+            master=self.window2,
             corner_radius=10,
             text_color='white',
             width=200,
             height=35
         )
-        self.entry2.place(x=210, y=340 )
+        self.entry2.place(x=75, y=120 )
 
         self.buttom1 = customtkinter.CTkButton(
-            master=self.window1,
+            master=self.window2,
             width=150,
             border_spacing=8,
             corner_radius=10,
@@ -60,7 +62,7 @@ class App(customtkinter.CTk):
             text_color='black',
             command=self.buttom_click
         )
-        self.buttom1.place(x=235, y=390)
+        self.buttom1.place(x=95, y=180)
 
 
 
